89b4d0a33dfd59a2a490091cbc1f486a2d8ff7b6
kazukashima <kazutorahattori@gmail.com>
2025-12-14 13:01:35 -0800

    commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -52,7 +52,7 @@ export default function AboutPage() {
 
     {/* 肩書き */}
     <p className="text-lg text-indigo-600 font-medium">
-      Full-Stack Developer
+      Frontend Developer
     </p>
 
   </header>
